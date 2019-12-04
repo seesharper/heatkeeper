de208273db5eb4b0935d5f7b47792d8b63803b58
Bernhard Richter <bernhard.richter@gmail.com>
2019-12-04 18:18:07 +0100

    Fixes docker build

---
+++ src/HeatKeeper.Server/Authentication/DefaultSecret.cs
@@ -0,0 +1,7 @@
+namespace HeatKeeper.Server.Authentication
+{
+    public static class DefaultSecret
+    {
+        public const string Value = "THIS IS THE DEFAULT SECRET. THIS MUST BE CHANGED IN A PRODUCTION ENVIRONMENT";
+    }
+}
