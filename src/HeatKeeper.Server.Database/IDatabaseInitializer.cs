08f00371a4a3080dbf264ab154c81c6ca6811926
Bernhard Richter <bernhard.richter@gmail.com>
2019-12-23 12:26:28 +0100

    Stop using FluentMigrator

--- src/HeatKeeper.Server.Database/IDatabaseInitializer.cs
+++ src/HeatKeeper.Server.Database/IDatabaseInitializer.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Data.SQLite;
 using DbReader;
-using FluentMigrator.Runner;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace HeatKeeper.Server.Database
 {
@@ -30,43 +28,6 @@ namespace HeatKeeper.Server.Database
                 connection.Execute(sqlProvider.CreateDatabase);
                 connection.Execute(sqlProvider.InsertAdminUser, new { Email = AdminUser.DefaultEmail, Firstname = AdminUser.DefaultFirstName, Lastname = AdminUser.DefaultLastName, HashedPassword = AdminUser.DefaultPasswordHash });
             }
-
-            // var serviceProvider = CreateServices(configuration.ConnectionString);
-
-            // // Put the database update into a scope to ensure
-            // // that all resources will be disposed.
-            // using (var scope = serviceProvider.CreateScope())
-            // {
-            //     UpdateDatabase(scope.ServiceProvider);
-            // }
-        }
-
-        private static IServiceProvider CreateServices(string connectionString)
-        {
-            return new ServiceCollection()
-                // Add common FluentMigrator services
-                .AddFluentMigratorCore()
-                .ConfigureRunner(rb => rb
-                    // Add SQLite support to FluentMigrator
-                    .AddSQLite()
-                    // Set the connection string
-                    .WithGlobalConnectionString(connectionString)
-                    // Define the assembly containing the migrations
-                    .ScanIn(typeof(DatabaseMigrator).Assembly).For.Migrations())
-                // Enable logging to console in the FluentMigrator way
-                .AddLogging(lb => lb.AddFluentMigratorConsole())
-                // Build the service provider
-                .BuildServiceProvider(false);
-        }
-
-
-        private static void UpdateDatabase(IServiceProvider serviceProvider)
-        {
-            // Instantiate the runner
-            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
-
-            // Execute the migrations
-            runner.MigrateUp();
         }
     }
 }
